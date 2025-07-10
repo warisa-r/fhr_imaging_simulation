f607163a950e5df5ac43d924863ad4d8e4150975
warisa-r <compund555@gmail.com>
2025-07-10 17:38:00 +0200

    TX and RX implementation

---
+++ 2d_RT_helmholtz_TX_RX/mesh_generation.py
@@ -0,0 +1,82 @@
+import sys
+
+from mpi4py import MPI
+
+import gmsh
+
+def add_disk(x, y, r):
+    circle = gmsh.model.occ.addCircle(x, y, 0.0, r)
+    loop = gmsh.model.occ.addCurveLoop([circle])
+    surface = gmsh.model.occ.addPlaneSurface([loop])
+    return surface
+
+def generate_mesh(filename, lmbda, order, receiver_pos, verbose = False):
+    if MPI.COMM_WORLD.rank == 0:
+        import gmsh
+        gmsh.initialize()
+        gmsh.model.add("helmholtz_domain")
+        gmsh.option.setNumber("General.Terminal", verbose)
+        
+        # Use direct mesh size instead of CharacteristicLengthFactor
+        mesh_size = lmbda / 16
+        gmsh.option.setNumber("Mesh.CharacteristicLengthMax", mesh_size)
+        gmsh.option.setNumber("Mesh.CharacteristicLengthMin", mesh_size / 4)
+        
+        # Turn off automatic mesh sizing to force our values
+        gmsh.option.setNumber("Mesh.CharacteristicLengthFromCurvature", 0)
+        gmsh.option.setNumber("Mesh.CharacteristicLengthFromPoints", 0)
+        gmsh.option.setNumber("Mesh.CharacteristicLengthExtendFromBoundary", 0)
+
+        delta = lmbda # A little edge so that the receiver doesn't stand exactly on BC
+        comp_domain_x_low = min(-0.2, receiver_pos[0])
+        comp_domain_dx = max(0.2, receiver_pos[0]) - comp_domain_x_low + delta
+        comp_domain_y_low = min(-0.15, receiver_pos[1])
+        comp_domain_dy = max(0.15, receiver_pos[1]) - comp_domain_y_low + delta
+        rect = gmsh.model.occ.addRectangle(comp_domain_x_low, comp_domain_y_low, 0.0, comp_domain_dx, comp_domain_dy)
+        
+        p0 = add_disk(0.0, 0.0, 0.1)
+        p1 = add_disk(-0.03, -0.015, 0.015)
+        p2 = add_disk(0.07, -0.015, 0.015)
+        p3 = add_disk(0.01, 0.05, 0.01)
+
+        all_surfaces = [(2, rect), (2, p0), (2, p1), (2, p2), (2, p3)]
+        gmsh.model.occ.fragment(all_surfaces, []) # From just surfaces, we put the fragments together
+        gmsh.model.occ.synchronize()
+
+        # We now need to assign each surface to be in different physical groups
+        # Because the geometry we define earlier collapse once we fragments thing together
+        rect_surf = None
+        p0_surf = None
+        p_metal_surfs = []
+        # Get all the different existing entities in the mesh
+        surfaces = gmsh.model.getEntities(2)
+        areas = [gmsh.model.occ.getMass(dim, tag) for dim, tag in surfaces]
+
+        # Find the largest area (background)
+        max_area_idx = areas.index(max(areas))
+        rect_surf = surfaces[max_area_idx][1]
+
+        # Find P0 (center near origin, but not the largest area)
+        p0_surf = None
+        p_metal_surfs = []
+        for i, (dim, tag) in enumerate(surfaces):
+            com = gmsh.model.occ.getCenterOfMass(dim, tag)
+            if i == max_area_idx:
+                continue  # NOT BACKGROUND
+            # P0: center near origin, radius ~0.1
+            if (com[0]**2 + com[1]**2)**0.5 < 0.01:
+                p0_surf = tag
+            else:
+                p_metal_surfs.append(tag)
+
+        gmsh.model.addPhysicalGroup(2, [rect_surf], tag=3)  # Background
+        if p0_surf is not None:
+            gmsh.model.addPhysicalGroup(2, [p0_surf], tag=1)    # P0
+        if p_metal_surfs:
+            gmsh.model.addPhysicalGroup(2, p_metal_surfs, tag=2)  # P1, P2, P3
+
+        gmsh.model.mesh.generate(2)
+        gmsh.model.mesh.setOrder(order)
+        gmsh.model.mesh.optimize("HighOrder")
+        gmsh.write(filename)
+        gmsh.finalize()
--- 2d_RT_helmholtz_TX_RX/simulation.py
+++ 2d_RT_helmholtz_TX_RX/simulation.py
@@ -1,6 +1,7 @@
 from mpi4py import MPI
 import sys
 
+import pandas as pd
 import numpy as np
 import scipy.io
 from scipy.special import hankel1
@@ -17,24 +18,39 @@ from dolfinx.fem import Function
 from dolfinx import fem
 
 from mesh_generation import generate_mesh
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from export_function import export_function
 
-pathname_calib = r'./data/'
+# MPI communicator
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-# Load the frequency vector
-freq_vec_init = scipy.io.loadmat(pathname_calib + 'freq_vec.mat')['freq_vec'].flatten()
-freq_vec = np.flip(freq_vec_init) # Flip to get up-chirp
+# Only rank 0 loads data and broadcasts
+if rank == 0:
+    pathname_calib = r'./data/'
+    
+    # Load the frequency vector
+    freq_vec = scipy.io.loadmat(pathname_calib + 'freq_vec.mat')['freq_vec'].flatten()
 
-# Load the antenna position
-ant_pos = np.loadtxt(pathname_calib + 'ant_pos.txt')
-ant_pos_2D = ant_pos[:, [0, 2]] # Take receiver position in x and z dims only
+    print(freq_vec)
+    # Load the antenna position
+    ant_pos = np.loadtxt(pathname_calib + 'ant_pos.txt')
+    ant_pos_2D = ant_pos[:, [0, 2, 3, 5]] # Take the transmitter and receiver positions in x and z dims only
+    
+    print(f"Loaded {len(freq_vec)} frequencies and {len(ant_pos_2D)} antennas")
+else:
+    freq_vec = None
+    ant_pos_2D = None
+
+# Broadcast data to all processes
+freq_vec = comm.bcast(freq_vec, root=0)
+ant_pos_2D = comm.bcast(ant_pos_2D, root=0)
 
 # Define some constants
 c = 299792458
-interested_f = freq_vec.min()
-lam0 = c / interested_f
-
-# wavenumber in free space (air)
-k0 = 2* np.pi * interested_f / c
 
 # Polynomial degree
 degree = 6
@@ -42,109 +58,180 @@ degree = 6
 # Mesh order
 mesh_order = 2
 
-# MPI communicator
-comm = MPI.COMM_WORLD
+def run_sim(freq, transmitter_pos, receiver_pos, visualize = False):
+    
+    lam0 = c / freq
+    print(lam0)
 
-# Generate the mesh
-file_name = "domain.msh"
-generate_mesh(file_name, lam0, order=mesh_order)
-mesh, cell_tags, _ = gmshio.read_from_msh(file_name, comm, rank=0, gdim=2)
+    # wavenumber in free space (air)
+    k0 = 2* np.pi * freq / c
 
-# Assigning material parameter
-W = dolfinx.fem.functionspace(mesh, ("DG", 0))
-# Function: A finite element function that is represented by a function space (domain, element and dofmap) and a vector holding the degrees-of-freedom.
-k = dolfinx.fem.Function(W) # k is a function whose values vary across the mesh (input: cell. output: value of k at that cell)
-k.x.array[:] = k0 # x here just represent the variable of a degree of freedom
+    file_name = f"domain_rank{rank}.msh"
+    generate_mesh(file_name, lam0, mesh_order, receiver_pos)
+    mesh, cell_tags, _ = gmshio.read_from_msh(file_name, comm, rank=0, gdim=2)
 
-# Set refractive index for differrent geometry withing the computational domain
-n_refractive = 1.5 # Refractive index for the refraction geometry
-n_refractive_metal = 500 # Refractive index for the metal inlets
-k.x.array[cell_tags.find(1)] = n_refractive_metal * k0 # Refraction geometry
-k.x.array[cell_tags.find(2)] = n_refractive_metal * k0
+    # Assigning material parameter
+    W = dolfinx.fem.functionspace(mesh, ("DG", 0))
+    k = dolfinx.fem.Function(W)
+    k.x.array[:] = k0
 
-# Visualize the mesh
-#pyvista.start_xvfb()
-pyvista.set_plot_theme("paraview")
-sargs = dict(
-    title_font_size=25,
-    label_font_size=20,
-    fmt="%.2e",
-    color="black",
-    position_x=0.1,
-    position_y=0.8,
-    width=0.8,
-    height=0.1,
-)
+    # Set refractive index for different geometry within the computational domain
+    n_refractive = 1.5 # Refractive index for the refraction geometry
+    n_refractive_metal = 500 # Refractive index for the metal inlets
+    k.x.array[cell_tags.find(1)] = n_refractive * k0 # Refraction geometry
+    k.x.array[cell_tags.find(2)] = n_refractive_metal * k0
 
+    # Define source term at boundary
+    n = ufl.FacetNormal(mesh)
 
-def export_function(grid, name, show_mesh=False, tessellate=False):
-    grid.set_active_scalars(name)
-    plotter = pyvista.Plotter(window_size=(700, 700))
-    t_grid = grid.tessellate() if tessellate else grid
-    plotter.add_mesh(t_grid, show_edges=False, scalar_bar_args=sargs)
-    if show_mesh:
-        V = dolfinx.fem.functionspace(mesh, ("Lagrange", 1))
-        grid_mesh = pyvista.UnstructuredGrid(*vtk_mesh(V))
-        plotter.add_mesh(grid_mesh, style="wireframe", line_width=0.1, color="k")
-        plotter.view_xy()
-    plotter.view_xy()
-    plotter.camera.zoom(1.3)
-    plotter.export_html(f"./{name}.html")
+    # UFL does not support hankel1, so use a Function for boundary data
+    def hankel_incident_eval(x):
+        r = np.sqrt((x[0] - transmitter_pos[0])**2 + (x[1] - transmitter_pos[1])**2)
+        r = np.where(r < 1e-12, 1e-12, r)
+        return hankel1(0, float(k0) * r)
 
-grid = pyvista.UnstructuredGrid(*vtk_mesh(mesh))
-grid.cell_data["wavenumber"] = k.x.array.real
-export_function(grid, "wavenumber", show_mesh=True, tessellate=True)
+    V = dolfinx.fem.functionspace(mesh, ("Lagrange", 1))
+    uinc = fem.Function(V)
+    uinc.interpolate(lambda x: hankel_incident_eval(x))
+    g = ufl.dot(ufl.grad(uinc), n) - 1j * k * uinc
 
-# Define source term at boundary
-n = ufl.FacetNormal(mesh)
-x = ufl.SpatialCoordinate(mesh)
+    # Define the weak form of the problem
+    element = basix.ufl.element("Lagrange", mesh.topology.cell_name(), degree)
+    V = dolfinx.fem.functionspace(mesh, element)
 
-# Use a cylindrical wave instead (2D case)
-#source_pos = ant_pos_2D[1]
-#r = ufl.sqrt((x[0] - source_pos[0])**2 + (x[1] - source_pos[1])**2)
-#uinc = ufl.exp(1j * k * r)/ ( 4 * np.pi * r)
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    # LHS
+    a = (
+        -ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
+        + k**2 * ufl.inner(u, v) * ufl.dx
+        - 1j * k * ufl.inner(u, v) * ufl.ds
+    )
+    L = ufl.inner(g, v) * ufl.ds # RHS
 
-source_pos = ant_pos_2D[0, :]
-x = ufl.SpatialCoordinate(mesh)
-r = ufl.sqrt((x[0] - source_pos[0])**2 + (x[1] - source_pos[1])**2)
+    opt = {"ksp_type": "preonly", "pc_type": "lu"}
+    problem = dolfinx.fem.petsc.LinearProblem(a, L, petsc_options=opt)
+    uh = problem.solve()
+    uh.name = "u"
 
-# UFL does not support hankel1, so use a Function for boundary data
-def hankel_incident_eval(x):
-    r = np.sqrt((x[0] - source_pos[0])**2 + (x[1] - source_pos[1])**2)
-    r = np.where(r < 1e-12, 1e-12, r)
-    return 100* hankel1(0, float(k0) * r) #TODO: Why does this work???
+    # Only visualize on rank 0 and only first simulation
+    if visualize and rank == 0:
+        # Visualize the mesh and wavenumber
+        pyvista.set_plot_theme("paraview")
+        sargs = dict(
+            title_font_size=25,
+            label_font_size=20,
+            fmt="%.2e",
+            color="black",
+            position_x=0.1,
+            position_y=0.8,
+            width=0.8,
+            height=0.1,
+        )
 
-V = dolfinx.fem.functionspace(mesh, ("Lagrange", 1))
-uinc = fem.Function(V)
-uinc.interpolate(lambda x: hankel_incident_eval(x))
-g = ufl.dot(ufl.grad(uinc), n) - 1j * k * uinc
+        grid = pyvista.UnstructuredGrid(*vtk_mesh(mesh))
+        grid.cell_data["wavenumber"] = k.x.array.real
+        export_function(mesh, grid, "wavenumber", show_mesh=True, tessellate=True)
 
-# Define the weak form of the problem
-# https://fenics.readthedocs.io/projects/ufl/en/latest/manual/form_language.html
-element = basix.ufl.element("Lagrange", mesh.topology.cell_name(), degree)
-V = dolfinx.fem.functionspace(mesh, element)
+        # Visualize the solution
+        topology, cells, geometry = vtk_mesh(V)
+        grid = pyvista.UnstructuredGrid(topology, cells, geometry)
+        grid.point_data["Abs(u)"] = np.abs(uh.x.array)
+        
+        # Add receiver position as a point
+        receiver_point = np.array([receiver_pos[0], receiver_pos[1], 0.0]).reshape(1, -1)
+        grid.point_data["receiver_pos"] = np.zeros(len(grid.points))
+        
+        # Find closest point to receiver and mark it
+        dist_to_receiver = np.linalg.norm(grid.points - receiver_point, axis=1)
+        closest_idx = np.argmin(dist_to_receiver)
+        grid.point_data["receiver_pos"][closest_idx] = 1.0
+        
+        export_function(mesh, grid, "Abs(u)", show_mesh=False, tessellate=True)
 
-u = ufl.TrialFunction(V)
-v = ufl.TestFunction(V)
-# LHS
-a = (
-    -ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
-    + k**2 * ufl.inner(u, v) * ufl.dx
-    - 1j * k * ufl.inner(u, v) * ufl.ds # ds represents the computational domain boundary
-)
-L = ufl.inner(g, v) * ufl.ds # RHS
+        grid.point_data["Re(u)"] = np.real(uh.x.array)
+        export_function(mesh, grid, "Re(u)", show_mesh=False, tessellate=True)
 
-opt = {"ksp_type": "preonly", "pc_type": "lu"}
-problem = dolfinx.fem.petsc.LinearProblem(a, L, petsc_options=opt)
-uh = problem.solve()
-uh.name = "u"
+        # Print receiver position info
+        print(f"Receiver position: {receiver_pos}")
+        print(f"Closest mesh point: {grid.points[closest_idx]}")
+        print(f"Distance to receiver: {dist_to_receiver[closest_idx]:.6f}")
 
-topology, cells, geometry = vtk_mesh(V)
-grid = pyvista.UnstructuredGrid(topology, cells, geometry)
-grid.point_data["Abs(u)"] = np.abs(uh.x.array)
-export_function(grid, "Abs(u)", show_mesh=False, tessellate=True)
+    # Find the signal strength at the receiver
+    dist = np.linalg.norm(mesh.geometry.x[:, :2] - receiver_pos, axis=1)
+    closest_node = np.argmin(dist)
+    signal = uh.x.array[closest_node]
+    return signal
 
-grid.point_data["Re(u)"] = np.real(uh.x.array)
-export_function(grid, "Re(u)", show_mesh=False, tessellate=True)
+# Initialize results matrix
+num_frequencies = len(freq_vec)
+num_antennas = len(ant_pos_2D)
+results_matrix = np.zeros((num_frequencies, num_antennas), dtype=complex)
 
-#TODO: https://undabit.com/2d-helmholtz-monopole-in-square-room
+if rank == 0:
+    print(f"Running {num_frequencies} frequencies × {num_antennas} antennas on {size} processes")
+
+# Distribute work across MPI processes
+# Each process handles a subset of antenna positions
+antennas_per_process = num_antennas // size
+remainder = num_antennas % size
+
+# Calculate start and end indices for this process
+start_antenna = rank * antennas_per_process + min(rank, remainder)
+end_antenna = start_antenna + antennas_per_process + (1 if rank < remainder else 0)
+
+# Process assigned antennas
+for j in range(start_antenna, end_antenna):
+    ant_pos_i = ant_pos_2D[j]
+    receiver_pos = ant_pos_i[2:4]
+    transmitter_pos = ant_pos_i[0:2]
+    
+    if rank == 0:
+        print(f"Process {rank}: Antenna {j+1}/{num_antennas}: Tx={transmitter_pos}, Rx={receiver_pos}")
+    
+    for i, freq in enumerate(freq_vec):
+        # Only visualize first frequency and first antenna on rank 0
+        visualize = (i == 0 and j == 0 and rank == 0)
+        
+        signal = run_sim(freq, transmitter_pos, receiver_pos, visualize=visualize)
+        results_matrix[i, j] = signal  # Store complex signal
+        
+        if i % 10 == 0 and rank == 0:  # Print progress every 10 frequencies
+            print(f"  Process {rank}: Frequency {i+1}/{num_frequencies}: {abs(signal):.6f}")
+    
+    if rank == 0:
+        print(f"  Process {rank}: Completed antenna {j+1}")
+
+# Gather results from all processes
+if rank == 0:
+    # Receive results from other processes
+    for r in range(1, size):
+        recv_data = comm.recv(source=r, tag=r)
+        for (i, j), signal in recv_data.items():
+            results_matrix[i, j] = signal
+else:
+    # Send results to rank 0
+    send_data = {}
+    for j in range(start_antenna, end_antenna):
+        for i in range(num_frequencies):
+            send_data[(i, j)] = results_matrix[i, j]
+    comm.send(send_data, dest=0, tag=rank)
+
+# Only rank 0 saves the results
+if rank == 0:
+    # Save complex values to CSV (real and imaginary parts)
+    df_real = pd.DataFrame(np.real(results_matrix))
+    df_imag = pd.DataFrame(np.imag(results_matrix))
+
+    df_real.index.name = 'Frequency_Index'
+    df_real.columns = [f'Antenna_{i+1}' for i in range(num_antennas)]
+
+    df_imag.index.name = 'Frequency_Index'
+    df_imag.columns = [f'Antenna_{i+1}' for i in range(num_antennas)]
+
+    df_real.to_csv('simulation_results_real.csv')
+    df_imag.to_csv('simulation_results_imag.csv')
+    
+    print("Results saved:")
+    print("- Real parts: 'simulation_results_real.csv'")
+    print("- Imaginary parts: 'simulation_results_imag.csv'")
