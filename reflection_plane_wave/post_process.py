bf9dc0cfbf7ea99352a7d1b3ef25941d7dcdc85f
Warisa <compund555@gmail.com>
2025-10-05 17:58:06 +0000

    Correct the result of amp2

--- reflection_plane_wave/post_process.py
+++ reflection_plane_wave/post_process.py
@@ -15,7 +15,7 @@ from HH_shape_opt import *
 frequency = 5e9
 inc_wave_setup = IncidentWaveSetup(frequency, plane_wave)
 
-measurement_data_file_path = "measurements/matlab_measurements_sin0.5_scatter.csv"
+measurement_data_file_path = "measurements/matlab_measurements_sin0.5_amp2.csv"
 msh_file_path = "meshes/square_with_rect_obstacle.msh"
 markers_dict = {
     "obstacle": OBSTACLE_MARKER,
@@ -28,17 +28,17 @@ obstacle_stiffness = 25
 initial_guess_mesh_util = MeshUtil(
     msh_file_path, markers_dict, obstacle_stiffness)
 
-result_path = "outputs/result_sin0.5_scatter_DG0_matlab.h5"
+result_path = "outputs/result_sin0.5_amp2_DG0_matlab.h5"
 goal_geometry_msh_path = "meshes/square_with_halfsin_perturbed_rect_obstacle.msh"
-matlab_fullfield_csv_path = "measurements/matlab_fullfield_sin0.5_scatter.csv"
+matlab_fullfield_csv_path = "measurements/matlab_fullfield_sin0.5_amp2.csv"
 results = calculate_magnitude_and_phase_error(matlab_fullfield_csv_path, result_path,
-                                        initial_guess_mesh_util, inc_wave_setup, True)
+                                        initial_guess_mesh_util, inc_wave_setup, False)
 
-plot_projected_errors(results, "outputs/error_sin0.5_scatter_DG0_matlab.png", True)
+plot_projected_errors(results, "outputs/error_sin0.5_amp2_DG0_matlab.png", False)
 plot_mesh_deformation_from_result(
     result_path,
     goal_geometry_msh_path,
     initial_guess_mesh_util,
-    plot_file_name="outputs/mesh_deformation_sin0.5_scatter_DG0_matlab.png",
-    mesh_overlay_plot_file_name = "outputs/mesh_overlay_sin0.5_scatter_DG0_matlab.png"
+    plot_file_name="outputs/mesh_deformation_sin0.5_amp2_DG0_matlab.png",
+    mesh_overlay_plot_file_name = "outputs/mesh_overlay_sin0.5_amp2_DG0_matlab.png"
 )
